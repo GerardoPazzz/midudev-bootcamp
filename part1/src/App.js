fe3a0b5e3292079e6ed4405f4aa7c0ca302dee73
GerardoPazzz <gerardo.paz.an333@gmail.com>
2022-12-21 17:03:03 -0500

    Agregando funcion Luu

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 const Description = (params) =>{
   return <p style={{color:params.color}}>Esta es la descripcion del curso de React</p>
 }
+const Luu = (params)=>{
+  return <p>Luciana, eres mi todo uwu</p>
+}
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Mensaje color="blue" message="Gerardo" age={22}/>
       <Mensaje color="red" message="Luciana" age={29}/>
       <Description color="green"/>
+      <Luu/>
     </div>
   );
 }
